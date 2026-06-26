33010cc112103eefe87b5aedf29b4a34372c7618
kafpliz <kafpliz@gmail.com>
2026-06-26 18:35:36 +0300

    change position post

--- src/controllers/push/push.service.ts
+++ src/controllers/push/push.service.ts
@@ -17,7 +17,7 @@ export class PushService {
   try {
       const [res, totalCount] = await Promise.all([
       this.prisma.notification.findMany({
-        orderBy: { createdAt: 'desc' },
+        orderBy: { createdAt: 'asc' },
         take: limit,
         skip: page == 1 ? 0 : limit * (page - 1)
       }),
